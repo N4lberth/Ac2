5d97ecb96031fdea29b4b7f9b7476bc868d92b6d
Nalberth <130379582+N4lberth@users.noreply.github.com>
2024-11-17 23:19:12 -0300

    mais dor

--- EP06/programa18.asm
+++ EP06/programa18.asm
@@ -1,9 +1,11 @@
-# Programa 17
+# Programa 18
 # Para a expressão a seguir, escreva um programa que calcule o valor de k:
-# k = x * y (Você deverá realizar a multiplicação através de somas!)
+# k = x^y
+# Obs: Você poderá utilizar o exercício anterior.
 # O valor de x deve ser lido da primeira posição livre da memória e o valor de y deverá lido da
-# segunda posição livre. O valor de k, após calculado, deverá ainda ser escrito na terceira posição
-# livre da memória.
+# segunda posição livre. O valor de k, após calculado, deverá ainda ser escrito na terceira posição livre da memória.
+# Dê um valor para x e y (dê valores pequenos !!) e use o MARS para verificar a quantidade de
+# instruções conforme o tipo (ULA, Desvios, Mem ou Outras)
 
 
 #inicio:
@@ -15,14 +17,15 @@ sll  $t0, $t0,   16      # t0 = 0x10010000;
 lw   $s0, 0($t0)         # x  = 10;
 lw   $s1, 4($t0)         # x  = 5;
 addi $t1, $zero, 0       # i  = 0;
+addi $s2, $zero, 1	 # k  = 1;
 
 loop:
-add  $s2, $s2,   $s0     # k = k + x;
+mul  $s2, $s2,   $s0     # k = x * x;
 addi $t1, $t1,   1 	 # i++;
-bne  $t1, $s1, loop      # for 1 = 0; i < y;
+bne  $t1, $s1,   loop    # for 1 = 0; i < y;
 
 sw   $s2, 8($t0)         # k = x * y
 
 .data
-x:  .word  10
+x:  .word  2
 y:  .word  5
