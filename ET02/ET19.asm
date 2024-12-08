9c33f25206f2527d2f311f8eaad69df475986ebe
Nalberth <130379582+N4lberth@users.noreply.github.com>
2024-12-08 17:32:08 -0300

    Não aguento mais

--- ET02/ET19.asm
+++ ET02/ET19.asm
@@ -23,3 +23,39 @@
 .text
 .globl main
 main:
+addi $t0, $zero, 0x1001  # t0  = 0x00001001;
+sll  $t0, $t0,   16      # t0  = 0x10010000;
+lw   $t1, 0($t0)         # A   = 23;
+lw   $t2, 4($t0)         # B   = 98;
+lw   $t3, 8($t0)         # C   = 17;
+
+slt  $t6, $t1,   $t2     # (t1 < t2) ? 1 : 0
+bne  $t6, $zero, prox1   # (t1 < t2) ? 1 : 0
+swap1:
+sw   $t2, 0($t0)	 # A = B
+sw   $t1, 4($t0)	 # B = A
+lw   $t1, 0($t0)         # A;
+lw   $t2, 4($t0)         # B;
+prox1:
+slt  $t6, $t2,   $t3     # (t2 < t3) ? 1 : 0
+bne  $t6, $zero, prox2   # (t2 < t3) ? 1 : 0
+swap2:
+sw   $t3, 4($t0)         # B = C
+sw   $t2, 8($t0)	 # C = B
+lw   $t2, 4($t0)         # B;
+lw   $t3, 8($t0)         # C;
+prox2:
+slt  $t6, $t1,   $t2     # (t1 < t2) ? 1 : 0
+bne  $t6, $zero, fim     # (t1 < t2) ? 1 : 0
+swap3:
+sw   $t2, 0($t0)	 # A = B
+sw   $t1, 4($t0)	 # B = A
+lw   $t1, 0($t0)         # A;
+lw   $t2, 4($t0)         # B;
+fim:
+sw   $t2, 12($t0)
+
+.data
+A: .word 9
+B: .word 98
+C: .word 9
